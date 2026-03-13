70498a5a1216e7b128d3166d836dc16c3aa27139
Aayush Rathore <156205640+Aayush-Rathore@users.noreply.github.com>
2026-03-13 13:10:15 +0530

    New robot.txt file generation logic added

--- app/robots.ts
+++ app/robots.ts
@@ -1,19 +1,34 @@
-import type { MetadataRoute } from "next";
-import { SITE_URL } from "@/lib/site";
+// import type { MetadataRoute } from "next";
+// import { SITE_URL } from "@/lib/site";
+
+// export default function robots(): MetadataRoute.Robots {
+//   return {
+//     rules: [
+//       {
+//         userAgent: "*",
+//         allow: "/",
+//       },
+//       {
+//         userAgent: ["GPTBot", "Google-Extended", "ClaudeBot", "CCBot"],
+//         allow: "/",
+//       },
+//     ],
+//     sitemap: `${SITE_URL}/sitemap.xml`,
+//     host: "blogs.codeprompt.in",
+//   };
+// }
+
+import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-      {
-        userAgent: ["GPTBot", "Google-Extended", "ClaudeBot", "CCBot"],
-        allow: "/",
-      },
-    ],
-    sitemap: `${SITE_URL}/sitemap.xml`,
-    host: "blogs.codeprompt.in",
-  };
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: [
+                '/api/'
+            ],
+        },
+        sitemap: 'https://blogs.codeprompt.in/sitemap.xml',
+    }
 }
